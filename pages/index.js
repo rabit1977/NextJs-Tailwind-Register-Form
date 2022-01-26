e11528dba91d2cb06ae153f12fdf37f67901db47
Rabit <rabit2245@gmail.com>
2022-01-26 23:58:41 +0100

    i have made some minor changes

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Register from '../components/Register';
 
 function App() {
   return (
-    <main className='flex flex-col  m-auto border-4 w-[300px]   justify-center items-center rounded-l text-white shadow-lg mt-8  py-8'>
+    <main className='flex flex-col  m-auto border-1 w-[300px] rounded-l text-white shadow-xl mt-8 px-6  py-6'>
       <Register />
     </main>
   );
